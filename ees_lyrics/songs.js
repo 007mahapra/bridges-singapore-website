75a8906d51d2a89d48c4529fe7b362d8ae73992a
Mahaveer Prajapati <007mahapra@gmail.com>
2026-08-01 13:31:10 +0800

    Add Songs for Ehsaas-E-Sukoon

---
+++ ees_lyrics/songs.js
@@ -0,0 +1,512 @@
+const songDatabase = [
+    {
+        id: 1,
+        title: "Oh Palanhare",
+        lyrics: `Oh Palanhare Nirgun aur nyare
+Oh Palanhare Nirgun aur nyare
+Tumre Bin Hamra kono naahi 
+Hamri uljhan suljhao bhagwan 
+Tumre Bin Hamra kono naahi 
+
+Tumhe hamka ho sambhale 
+Tum hain Hamre rakhwale
+Tumre Bin Hamra kono naahi 
+Tumre Bin Hamra kono naahi 
+Tumre Bin Hamra kono naahi 
+
+Chanda main tumhei bhare ho chandani
+Suraj main ujala tumhein se
+Yeh gagan hai magan 
+Tumhein to diye ho essey taarey
+Bhagwan ye jiwan tumhein na sanwarogey
+To kia koi sanwarey
+
+Oh Palanhare Nirgun aur nyare
+Tumre Bin Hamra kono naahi 
+Tumre Bin Hamra kono naahi 
+
+Jo Suno toh kahein, prabhu ji hamri hai binti
+Dukhijan ko dhiraj do
+Haare nahi wo kabhi dukh se 
+Tum nirbal ko raksha do 
+Reh paayain nirbal sukh se 
+Bhakti ko shakti do 
+Jag ke jo swami ho, Itni to araj suno 
+Hain path mei andhiyare
+De do vardan main ujiyaarey
+
+Oh Palanhare Nirgun aur nyare
+Tumre Bin Hamra kono naahi 
+Hamri uljhan suljhao bhagwan 
+Tumre Bin Hamra kono naahi 
+
+Oh Palanhare Nirgun aur nyare
+Tumre Bin Hamra kono naahi 
+Hamri uljhan suljhao bhagwan 
+Tumre Bin Hamra kono naahi 
+
+Oh Palanhare Nirgun aur nyare
+Tumre Bin Hamra kono naahi 
+Hamri uljhan suljhao bhagwan 
+Tumre Bin Hamra kono naahi`
+    },
+    {
+        id: 2,
+        title: "Mere Rehbara",
+        lyrics: `Bigdi banai tune , Seene se lagaya tune x2
+Baat Samjhayi tuney, mere rehbara x3
+Rehbara, rehabra , mere rehbara …
+
+Kaise karun ye bayan, kitna hai mujhpe karam
+Jabse hua hun main tera, mit gaye hain saare bharam
+kaise karun ye bayan..  mit gaye hain saare bharam
+mit gaye hain saare bharam
+haaan.. . 
+
+Kaisi muskan teri, badi unchi shaan teri, Kirpa apaar teri.. 
+Mere rehbara … x3
+
+Andhera thi yeh zindagi, tuney hain raushan kia
+Ab meri har saans ka, tu hi hai malik piya x2 
+Andhera thi yeh zindagi….....tu hi hai malik piya 
+tu hi hai malik piya
+haaan.. 
+
+Jine ki wajah meri, yadain aatha teri
+Tu hi hai dua meri, mere rehbara x3
+Rehbara, rehabra , mere rehbara …`
+    },
+    {
+        id: 3,
+        title: "Mangal Bhawan Amangal Haari",
+        lyrics: `Mangal bhawan amangal haari, Drevhu Su Dasarath ajar bihari 
+Ram Siya Ram , Siya Ram jai jai ram 
+
+Hoi hai soi jo ram rachi rakha, ko kari tark badhaaye saaka
+Dheeraj dharam mitra aru nari, aapd kaal parkhai chaari 
+
+Ho.. Jehi Ke jehi par satya sanehu , so tohey mile na kuch sandehu 
+Ho.. Jaki rahi bhawna jaisi, prabhu murat dekhi teen taisi 
+
+Raghukul reet sada chali aayi, pran jaye par vachan na jayi
+Hari anant hari katha ananta, kahihi sunihi baubidhi sab santa
+Sita Ram charit ati paawan,madhur sars aru ati manbhawan 
+
+Ho.. Bin satsang vivek na hoi, ram kripa bin sulabh na koi
+Ho.. Bheed padi bhakton ne pukara, aaao haro prabhu kashat hamara`
+    },
+    {
+        id: 4,
+        title: "Achutum Keshavam",
+        lyrics: `Achutum keshavam Krishan daamodarm, Ram narayanam jaanki balumbham
+Kaun kehte hain bhagwaan aate nahi, tum meera ke jaise bulate nahi
+Kaun kehte hain bhagwaan khate nahi, Ber shabri ke jaise khilate nahi
+Kaun kehte hain bhagwaan sotey nahi, maa yashoda ke jaise sulate nahi 
+Kaun kehte hain bhagwaan nachte nahi, gopiyon ki tarah tum nachte nahi
+Naam japte chalo kaam karte chalo, har smaye kirshan ka dhyan dharte chalo.`
+    },
+    {
+        id: 5,
+        title: "Shri Krishan Gobind Hare Murari",
+        lyrics: `Shri Krishan Gobind Hare murari, hey naath narayan vaasudeva 
+Pitar-maat swami sakha hamare,  hey naath narayan vaasudeva 
+
+Shri Krishan Gobind Hare murari, hey naath narayan vaasudeva 
+Thakur hamare praano se pyare, hey naath narayan vaasudeva 
+
+Shri Krishan Gobind Hare murari, hey naath narayan vaasudeva 
+Tum ho hamare, hum hain tumhare,  hey naath narayan vaasudeva 
+
+Shri Krishan Gobind Hare murari, hey naath narayan vaasudeva 
+Tumse hai dharti, tumse hai ambar, Agni- pawan aur saaton smandar 
+
+Shri Krishan Gobind Hare murari, hey naath narayan vaasudeva 
+
+Gobind bolo hari gopal bolo, Radha Raman Hari Gobind bolo 
+Hare Rama, Hare Rama, Rama Rama.. Hare Hare 
+Hare Krishna , Hare Krishna Krishna,Krishna Hare Hare`
+    },
+    {
+        id: 6,
+        title: "Keejo Kesari Ke Laal",
+        lyrics: `Keejo kesari ke laal, mera chota sa yeh kaam 
+Kardo kesari ke laal, mera chota sa yeh kaam 
+Meri Ram ji se keh dena jai siya ram
+
+Main Ram sang japta, tumhara sada naam 
+Apne ram ji se keh dena jai siya ram x2
+Kardo kesari ke laal, Meri Ram ji se keh dena jai siya ram
+
+Deen heen ke sahare, mahaveer tum ho 
+Apne bhakton ki jagaate , taqdeer tum ho
+Apne bhakton ki jagaate , taqdeer tum ho
+Har dukhia ka haath, tum lete ho thaam
+Meri Ram ji se keh dena jai siya ram
+
+Main Ram sang japta, tumhara sada naam 
+Kardo kesari ke laal, Meri Ram ji se keh dena jai siya ram
+
+Mahabali mahayodha mahasant tum ho
+Laate sookhe hue bagon mein basant mein tum ho 
+Laate sookhe hue bagon mein basant mein tum ho 
+Teri bhakti se aatma ki milta aaram
+Meri Ram ji se keh dena jai siya ram
+Apne ram ji se keh dena jai siya ram
+Kardo kesari ke laal, Meri Ram ji se keh dena jai siya ram
+
+Poori sada hi hamari har aas karna 
+Baba bhakton ko kabhi na niraash karna 
+Hanumat bhakton ko kabhi na niraash karna 
+Dono charan tumhare, hamare sukhdhaam
+Meri Ram ji se keh dena jai siya ram
+Apne ram ji se keh dena jai siya ram x2
+
+Main Ram sang japta, tumhara sada naam 
+Apne ram ji se keh dena jai siya ram x2
+Kardo kesari ke laal, Meri Ram ji se keh dena jai siya ram`
+    },
+    {
+        id: 7,
+        title: "Mera Piya Ghar Aaya",
+        lyrics: `Ajj Da Roz mubarak chadheya , maahi mere wehade wadeya 
+Ghadiyal devo nikalni, mera piya ghar aaya
+Mera piya ghar aaya , ho laal ni 
+Piya ghar aaya saanu allah milaya , Hun hoya fazal kamaal ni 
+Mera piya ghar aaya , ho laal ni 
+
+Chanda bhi saath laya, taare bhi saath laya , pagal banane aaya wo.. 
+Mera piya ghar aaya , ho ram ji .. 
+Laya barat laya, ghunghta uthane aaya , apna banane aaya wo.. 
+Mera piya ghar aaya , ho laal ni 
+
+Ghadiyal devo nikalni, mera piya ghar aaya
+Mera piya ghar aaya , ho laal ji
+Piya ghar aaya saanu allah milaya
+Hun aape karda nihaal ni, mera piya ghar Aaya
+
+Ghadi ghadi ghadiyal bajawe, Rain wasal di pia katave
+Mere dil di baat jo paave, mere man ki baat na jaane 
+Haton ja suto ghadiyal ni, mera piya ghar aaya 
+
+Daata piya ki sej nayari,ni main taran haaro vaari daata miliaya hun aa gayi wari
+hun wichdan hoya muhaal ni, mera piya ghar aaya …`
+    },
+    {
+        id: 8,
+        title: "Chaap Tilak Sab Ccheeni",
+        lyrics: `Apni Chab banaike main, jab piya ke paas gayi
+Jab Chab dekhi piya ki, So apni bhool gayi 
+Mosei naina haayi, Mosei naina x4
+Mosei naina milaike, Mosei naina milaike x2
+Chaap tilak sab ccheeni re mosei naina milaike
+
+Usne poocha ki katal hona hai,maine usse kaha ki ankhon se
+Itna kehte hi katal kar dala, usne ankhain milake aankhon se
+Mosei naina milaike, Mosei naina milaike x5
+
+Saiyan nikas gaye main na ladi thi, main na ladi thi wo kaisi ghadi thi
+Li na palat ke mori khabariya, aise sajjan se main kunwari bhali thi
+Saiyan nikas gaye main na ladi thi, main ladi thi, ladi thi , ladi thi
+Main na ladi thi wo kaisi ghadi thi, ghadi thi , ghadi thi
+Saiyan nikas gaye main na ladi thi, main ladi thi, ladi thi , ladi thi
+
+Wo gaye balam, wo gaye , jo nadi kanaar kanaar x2
+Aap to balam paar basey, hamno choda majhdhaar
+Mosei naina milaike, Mosei naina milaike x5
+
+Na gam do khuda ke liye shaad karke, kahan chal diye mujhko barbaad karke
+Main barbaad hona ganwara karunga, tu khush hai agar mujhko barbaad karke
+Sitaro tum hi unse jakar ke kehdo , koi ro raha hai tumhe yaad karke 
+Mosei naina milaike, Mosei naina milaike x5`
+    },
+    {
+        id: 9,
+        title: "Dama Dam Mast Kalandar",
+        lyrics: `Oh Laal muhinji pat rakhijan bhala jhoole laalan 
+Sindhdiye ja, sewan ja , sakhi shahbaz kalandar 
+Dama dam mast kalandar, Sindh ja pehla number 
+Gule-gulzaar kalandar, laal jo pehedeo number 
+Dama dam mast kalandar , sakhi shahbaz kalandar 
+
+Char Chirag tuhinja baran hamesha panjwan main baran aayi bhala jhoole laalan 
+Sindhdiye ja, sewan ja , sakhi shahbaz kalandar 
+Dama dam mast kalandar , sakhi shahbaz kalandar 
+Mata unjhi jhulan jhol bharinde
+Niadhian ja kandhe, bhag bhala, jhoole laalan
+
+Hind Sindh peera tuhinji naubat baaje
+naale alakh je bedo taar bhala, jhoole laalan 
+Peeran ja peer muhinja, mor acnu seer mein
+naale alakh je bedo taar, bhala jhoole laalan 
+
+Shah wassan dar jeko indho, man jiun muradon paaye wendu 
+tin ja katinde kashat bhala, jhoole laalan
+Dulhe tuhinji jain jyot jagayi
+Tinja bharin bhandar  bhala, jhoole laalan`
+    },
+    {
+        id: 10,
+        title: "Sanson Ki Mala Pe Simrun Main",
+        lyrics: `Aaa piya en nainan mei, main palak dhanp tohe lun
+Na mai dekhun gair ko, na tohe dekhan doon
+Ab kismat ke hath hain es bandhan ki laaj
+maine to sar rakh diya sanwariya ke paas
+Hath pakad pi ne kaha , pyari kuch to maang
+Sada raho meri maang mein, yahi hai meri maang
+
+Sanson ki mala pe simrun main Shiv ka naam 
+Sabke andar shiv hi rehte hain, karlo pehchaan 
+Sanson ki mala pe simrun main Shiv ka naam
+
+Har prani ka mol samjhna, yeh sab hai anmol
+Shiv hi hai har prani mei, har prani shiv ka dhaam 
+Gaan karun mei kaise shiv ka, mere bas ka na kaam 
+Sachhe karmo ki sewa hi, hogi parvaan 
+
+Shiv ke rang mein aisi doobi, ban gaya ek hi roop
+Har har har har japte japte, beete har ek saans
+Prem ka pyala de do aisa, Ji ka ho aisa haal
+Angaron pe neend aajaye, kaanton pe araam
+Jiwan ka shringar hai pritam, Maang ka hai sindoor
+Pritam ke naam se jana jaye , ab mera naam`
+    },
+    {
+        id: 11,
+        title: "Khuda Ki Baat Hai Sukoon",
+        lyrics: `Khuda ki Baat Hai Sakoon
+Khuda ki Baat Hai Sakoon, Khuda ki Zaat Hai Sakoon
+Satguru Ne Rabb Dikhalaya Hai, Khuda Ka Ghar Dikhalaya Hai
+Jisme Kayanaat Banai hai, Khuda Usi Me Samaya hai,  
+Khuda ki Baat Hai Sakoon,  Khuda ki Zaat Hai Sakoon
+
+Na Beete Kal Me Jeena Hai, Na Agle Pal Me Jeena Hai
+Khuda ko Paakar Murshid se, Hume Har Pal Me Jeena hai
+Achha Hi Ho Raha Hai Sab, Ye Guru Ne Hi Samjhaya Hai 
+Satguru Ne Rabb Dikhlaya Hai
+
+So lets walk on the path of humanity
+And rise above all disparities
+Shed our egos and spread love
+With god knowledge ,let's remember
+Nirankar,is the ultimate reality
+It's the only way to attain inner peace
+Words of satguru,  Will guide you 
+And in your heart,you'll find Sukoon.
+
+Khuda ki Baat Hai Sakoon, Khuda ki Zaat Hai Sakoon
+Kandha Nu Dhaah Riha Aye Jo Vich Dilaan De 
+Eh Pyar Paa Riha aye Jo vich Dilaan De
+Eh Ikk Dikha Riha Aye, Eh karam Ehna De 
+Khuda ki Baat Hai Sakoon, Khuda ki Zaat Hai Sakoon`
+    },
+    {
+        id: 12,
+        title: "Saanu Tera Hi Sahara",
+        lyrics: `Daata mere tere karke, zindagi vich baharan ne.. X2
+Jittan vich tabdeel ho gayian,meriyan saariyan haaran ne 
+Ke rooh nu sukoon aagaya , x2  tera milya ishaara eh.. 
+Saanu tera hi sahara eh.. Tere dar te najara eh.. Saanu tera hi sahara eh.. 
+
+Charna vich ardas tere eh, baa na saadi chaddin tun x2
+Jinney vi ah avgun mere, ek ek karke kaddin tun
+Tere bina kaun jaanda x2 daata haal mera saara eh.. 
+Saanu tera hi sahara eh.. Tere dar te najara eh.. Saanu tera hi sahara eh..
+
+Jad de tere hoa gaye sahiba, maza agaya jeoney da x2
+Sach dassan tan mul pai giya, dunia utte auney da
+Si Dubda pia lahauria, x2 Tu tan miliya kinaara eh..
+Saanu tera hi sahara eh.. Tere dar te najara eh.. Saanu tera hi sahara eh.. 
+
+Sewa Simran satsang wali, daat tun jholi paayi eh
+Aakhri saah tak karde rahiye, eho asal kamai eh
+Mei aaj tak jo mangya x2 na unha laya kade laara eh..
+Saanu tera hi sahara eh.. Tere dar te najara eh.. Saanu tera hi sahara eh..`
+    },
+    {
+        id: 13,
+        title: "Tu Maane Ya Na Maane Dildara",
+        lyrics: `Bulleh shah ji shear--
+Rabb bande di zaat hai ekko, te jiwei kapde di zaat hai roon..
+Kapde de vich roon hai lukeya, kitey vich bande de tun.. 
+Aape bolein te aap bulawein, aap karain hun hun..
+
+shear---- 
+Surat hai ek jaisi kisko juda kahun,
+Ya tumko khuda kahun,  ya khuda ko khuda kahun
+Tu maane ya na maane dildara asaan te tenu rabb manya 
+Dass hor kehda rabb da duara. Asaan te tenu rabb manya.. 
+
+Tujh bin jeena bhi kya jeena, teri chaukhat mera madina  x2
+Tujh bin jeena kya jeena,
+Tujh bin jeena kya jeena,x4
+Tujh bin jeena bhi kya jeena, teri chaukhat mera madina 
+Kahin aur na sajda gawara asaan te tainu rabb manya 
+Tu maane ya na maane dildara asaan te tenu rabb manya
+
+Kisi ke naak main heera kisi ke kaan main heera
+Hamain heeron se maltab kia, mera toh yaar hai heera.. 
+
+Apne tan ki khaak udai, tab yeh ishq ki manjil paayi  x2
+Meri saanson ka bole ek tara asaan te tenu rabb manya..
+Tu maane ya na maane dildara asaan te tenu rabb manya 
+
+Haste haste har gham sehna, razi teri raza main rehna..x2
+Tuney mujhko sikhaya hai yara, asaan te tenu rabb manya..
+Tu maane ya na maane dildara asaan te tenu rabb manya`
+    },
+    {
+        id: 14,
+        title: "Guru Meri Bhakti Guru Meri Pooja",
+        lyrics: `Guru meri bhakti, guru meri pooja 
+Guru vishwaas hai, guru vishwaas hai 
+Guru chanro main, rahun mein hamesha
+Yehi meri aas hai, guru vishwaas hai.. 
+
+Guru Brahma hai, guru Vishnu hai x2
+Shish ki nigahon mein, guru hi prabhu hai x2
+Sukhon ko woh lootey, dukhon se who chootey x2
+Man se bachan se, Guru ka jo daas hai
+
+Sotey ko jagaye, rotey ko hasaaye x2
+Guru sabko al main, ram se milaye x2
+Mera man hai kaala, guru hai ujala x2
+Andhere jagat main, karta prakash hai
+
+Guru meri bhakti, guru meri manjil
+Guru door karta, "Naath" har mushkil
+Main hun ek gagar, guru gyan sagar kare sab pe chaya, guru wo prakash hai`
+    },
+    {
+        id: 15,
+        title: "Mere Tan Mein Bhi Radha",
+        lyrics: `Mere tan mein bhi Radha, mere man mein bhi Radha
+Mere kan-kan mein Radha, mere man-man mein Radha
+Rom-rom mein samaya Radha naam re
+Meri saanson mein hai Radha naam re, haay meri saanson...
+
+Jaise chanda mein Radha, jaise sooraj mein Radha
+Ambar taaron mein samaya tera naam re
+Haay meri saanson mein hai Radha...
+
+Jaise Krishna mein Radha, jaise banshi mein Radha
+Mor pankh mein samaya Radha naam re
+Haay meri saanson mein hai Radha...
+
+Jaise Barsana mein Radha, jaise Vrindavan mein Radha
+Braj bhoomi mein samaya Radha naam re
+Haay meri saanson mein tera...
+
+Mere hriday mein Radha, meri dhadkan mein Radha
+Antaraatma mein samaya Radha naam re
+Haay meri saanson mein hai Radha...
+
+Jaise chandan mein Radha, jaise tulsi mein Radha
+Kan-kan mein samaya Radha naam re
+Haay meri saanson mein hai Radha...
+
+Jaise pooja mein Radha, jaise aarti mein Radha
+Mandir ki moorat mein samaya tera naam re
+Haay meri saanson mein hai Radha...
+
+Jaise phoolon mein Radha, jaise patton mein Radha
+Saari srishti mein samaya Radha naam re
+Haay meri saanson mein hai Radha...
+
+Mere tan mein bhi Radha, mere man mein bhi Radha
+Mere kan-kan mein Radha, mere man-man mein Radha
+Rom-rom mein samaya Radha naam re
+Haay meri saanson mein hai Radha naam re.`
+    },
+    {
+        id: 16,
+        title: "Bhajan Wali Mala",
+        lyrics: `Jiske japne se mil jaaye Nandlala, jaake japne se mil jaaye Nandlala
+O mujhe laa do bhajan wali wohi mala
+Woh mala Yashoda ne pheri
+Dekho leela dikha gaye Nandlala, mukh mein leela dikhla di
+O mujhe laa do bhajan wali wohi mala
+
+Jaake japne se mil jaaye Nandlala
+Woh mala Kaushalya ne pheri
+Ayodhya mein aa gaye Ram Lala
+O mujhe laa do bhajan wali wohi mala
+
+Woh mala Gauraan ne pheri
+Bhole Shankar ko pehna di varmala
+O mujhe laa do bhajan wali wohi mala
+
+Jo mala Draupadi ne pheri
+Dekho cheer badha gaye Nandlal
+O mujhe laa do bhajan wali wohi mala
+
+Jo mala Hanuman ji ne pheri
+Jo mala Hanuman ji ne pheri
+Dekho Lanka ko pal mein jala daala
+O mujhe laa do bhajan wali wohi mala`
+    },
+    {
+        id: 17,
+        title: "Prabhu Ji Mujhko Bhool Gaye Kya",
+        lyrics: `Prabhu ji mujhko bhool gaye kya
+Prabhu ji mujhko bhool gaye lo, daas ki khabariya
+Rama Rama ratte ratte beeti re umariya
+Raghukul Nandan kab aaoge Bhilni ki nagariya
+
+Main Shabari Bhilni ki jaayi, bhajan bhaav na jaanu re
+Ram tere darshan ke hit van mein jeevan paalun re
+Charan kamal se nirmal kar do, daas ki jhopadiya
+Rama Rama ratte ratte beeti re umariya
+Raghukul Nandan kab aaoge Bhilni ki nagariya
+
+Roz savere van mein jaakar, phal chun chun kar laaungi
+Apne Prabhu ke sanmukh rakh ke, prem se bhog lagaungi
+Meethe meethe beron ki main, bhar laayi chhabariya
+Rama Rama ratte ratte beeti re umariya
+Raghukul Nandan kab aaoge Bhilni ki nagariya
+
+Shyaam saloni mohini moorat, nainan beech basaungi
+Pad pankaj ki raj dhar mastak, jeevan safal banaungi
+Ab kya Prabhu ji bhool gaye ho, daas ki dagariya
+Rama Rama ratte ratte beeti re umariya
+Raghukul Nandan kab aaoge Bhilni ki nagariya
+
+Naath tere darshan ki pyaasi, main abla ik naari hoon
+Darshan bin do naina tarasen, sun lo bahut dukhari hoon
+Ab to humko darshan de do, daalo ik nazariya
+Rama Rama ratte ratte beeti re umariya
+Raghukul Nandan kab aaoge Bhilni ki nagariya`
+    },
+    {
+        id: 18,
+        title: "Har Har Shambhu Shiv Mahadeva",
+        lyrics: `॥Har Har Shambhu
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.
+
+Karpoora Gauram Karunaavataram Sansaara Saaram Bhujagendra Haaram.
+Sadaa Vasantam Hridayaaravinde Bhavam Bhavaani Sahitam Namaami.
+
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.
+
+Saananda Maananda Vane Vasantam Aananda Kandam Hata Paapa Vrindam.
+Vaaraanasi Naatham Anaatha Naatham Shri Vishwanaatham Sharanam Prapadye.
+
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.
+
+Avantikaayaam Vihitaavataaram Mukti Pradaanaaya Cha Sajjanaanaam.
+Akaala Mrityoh Parirakshanaartham Vande Mahaakaala Mahaasuresham.
+
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.
+
+Naagendra Haaraaya Trilochanaaya Bhasmaanga Raagaaya Maheshwaraaya.
+Nityaaya Shuddhaaya Digambaraaya Tasmai Nakaraaya Namah Shivaaya.
+
+Har Har Shambhu, Shambhu, Shambhu, Shambhu Shiv Mahadeva,
+Shambhu, Shambhu, Shambhu, Shambhu, Shiv Mahadev.`
+    }
+];
